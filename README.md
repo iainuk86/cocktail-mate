f83bb4d9e285b4756cddc7122b65e87af4096eb4
iain <iainuk1986@gmail.com>
2024-09-03 21:30:02 +1000

    Add package: pinia-plugin-persistedstate

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# 🍸 Cocktail Mate 🍸
+# 🍸 Cocktail Mate
 
 Cocktail Mate is a web service providing great recipes and cocktail advice.  
 It was built with an E-commerce style and was a great way for me to learn the Vue framework.
 
 Link to current hosted site - https://cocktails.majatech.net/
 
-# ⚒️ Implementation ⚒️
+# ⚒️ Implementation
 Started life as a traditional Bootstrap / HTML / CSS / JS web template.  
 It was then migrated for use in a Spring Boot app, making use of Thymeleaf templates.  
 Now it has been migrated to use mainly client-side rendering with the Vue framework.  
 
-# 🔜 Coming Soon ⚠️
+# 🔜 Coming Soon
 - Implement an AI Chatbot to receive various prompts and recommend cocktails for the ingredients / weather at hand.
 - A more responsive design for mobile users.  
--- src/stores/cocktails.ts
+++ src/stores/cocktails.ts
@@ -6,16 +6,20 @@ export const useCocktailsStore = defineStore('cocktails', () => {
     const cocktails = ref<Cocktail[]>([])
     const currentCocktails = ref<Cocktail[]>([])
     const activeCocktail = ref<Cocktail>({
-        cocktailId: 'placeholder',
-        name: 'placeholder',
-        flavour: 'placeholder',
-        ingredients: 'placeholder',
-        method: 'placeholder',
-        glass: 'placeholder',
-        ice: 'placeholder',
-        garnish: 'placeholder',
-        spirit: 'placeholder',
+        cocktailId: '',
+        name: '',
+        flavour: '',
+        ingredients: '',
+        method: '',
+        glass: '',
+        ice: '',
+        garnish: '',
+        spirit: '',
     })
   
     return { cocktails, currentCocktails, activeCocktail }
+}, {
+    persist: {
+        storage: sessionStorage
+    }
 })
